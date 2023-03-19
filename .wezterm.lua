7ba272c3a625900ebf9bae1846d869b4dee4cd44
Mbechezi Mlanawo <mlanawo.mbechezi@alpacode.io>
2023-03-19 09:46:36 +0100

    Add terminator commands

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,17 +1,29 @@
-local wzeterm = require 'wezterm'
+local wezterm = require 'wezterm'
 
 return {
-    enable_wayland = true,
-    font_size = 13,
-    enable_tab_bar = true,
-    keys = {
-    	{
-      	   key = '"',
-      	   mods = 'CTRL|SHIFT|e',
-           action = wezterm.action.SplitVertical {
-              args = { 'left' }
-           }
-       }
-    }  
+  enable_wayland = true,
+  font_size = 13, 
+  enable_tab_bar = true, 
+  keys = {
+    -- This will create a new split and run the `top` program inside it
+    {
+      key = 'e',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.SplitVertical {
+        domain = 'CurrentPaneDomain'
+      },
+    },
+    {
+        key = 'o',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitHorizontal {
+          domain = 'CurrentPaneDomain'
+        },
+    },
+    {
+        key = 'x',
+        mods = 'SHIFT|CTRL',
+        action = wezterm.action.TogglePaneZoomState,
+    },
+  },
 }
-
