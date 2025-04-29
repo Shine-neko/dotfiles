a1cd69df02e27ba8cd06e19d576efad255317c44
Mlanawo MBECHEZI <mlanawo.mbechezi@alpacode.io>
2025-04-29 12:34:46 +0300

    Upgrade to composer 2.8

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -3,4 +3,4 @@ FROM php:8.3-fpm
 RUN apt update && apt install -y git zip unzip wget
 RUN wget https://get.symfony.com/cli/installer -O - | bash
 
-COPY --from=composer:2.1 /usr/bin/composer /usr/bin/composer
+COPY --from=composer:2.8 /usr/bin/composer /usr/bin/composer
